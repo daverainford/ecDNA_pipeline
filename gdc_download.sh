bc6a34b7190c147219478005b5c25a7312eb3df2
daverainford <rainford.d.k@gmail.com>
2024-07-29 14:01:09 -0700

    Gave 'gdc_download.sh' more resources for faster download speeds.

--- gdc_download.sh
+++ gdc_download.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL     
 #SBATCH --mail-user=drainford@tgen.org  
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=20                                    
+#SBATCH --cpus-per-task=40                                    
 #SBATCH --time=36:00:00               
 #SBATCH --output=/scratch/drainford/logs/gdc_client.log
 
@@ -12,10 +12,10 @@ module load singularity
 
 # Download TCGA data to data download directory
 singularity exec /scratch/drainford/containers/gdc-client_1.6.1.sif gdc-client download \
-    -n 20 \
-    -m download_data/*batch* \
+    -n 40 \
+    -m download_data/*manifest* \
     -t download_data/*token* \
-    -d /scratch/drainford/ecDNA/data/
+    -d /scratch/drainford/ecDNA_pipeline/data/
 
 # Empty all subdirectories in data download directory
 find /scratch/drainford/ecDNA/data/ -mindepth 2 -type f -exec mv -t /scratch/drainford/ecDNA/data/ {} +
@@ -25,6 +25,3 @@ find /scratch/drainford/ecDNA/data/ -mindepth 1 -type d -exec rm -r {} +
 
 # Remove uneeded index and parcel files
 rm /scratch/drainford/ecDNA/data/*.bai /scratch/drainford/ecDNA/data/*.parcel
-
-# # Get md5sums for bams all files in download directory
-# md5sum /scratch/drainford/ecDNA/data/* > checksums.txt
