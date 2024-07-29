bef138934449827b8fed08d25fed430973e705b1
daverainford <rainford.d.k@gmail.com>
2024-07-29 14:04:48 -0700

    Gave 'gdc_download.sh' more walltime.

--- gdc_download.sh
+++ gdc_download.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=drainford@tgen.org  
 #SBATCH --ntasks=1 
 #SBATCH --cpus-per-task=40                                    
-#SBATCH --time=36:00:00               
+#SBATCH --time=96:00:00               
 #SBATCH --output=/scratch/drainford/logs/gdc_client.log
 
 # Load singularity
@@ -15,13 +15,13 @@ singularity exec /scratch/drainford/containers/gdc-client_1.6.1.sif gdc-client d
     -n 40 \
     -m download_data/*manifest* \
     -t download_data/*token* \
-    -d /scratch/drainford/ecDNA_pipeline/data/
+    -d /vscratch/drainford/ecDNA_data/
 
 # Empty all subdirectories in data download directory
-find /scratch/drainford/ecDNA/data/ -mindepth 2 -type f -exec mv -t /scratch/drainford/ecDNA/data/ {} +
+find /vscratch/drainford/ecDNA_data/ -mindepth 2 -type f -exec mv -t /scratch/drainford/ecDNA/data/ {} +
 
 # Delete the now empty directories
-find /scratch/drainford/ecDNA/data/ -mindepth 1 -type d -exec rm -r {} +
+find /vscratch/drainford/ecDNA_data/ -mindepth 1 -type d -exec rm -r {} +
 
 # Remove uneeded index and parcel files
-rm /scratch/drainford/ecDNA/data/*.bai /scratch/drainford/ecDNA/data/*.parcel
+rm /vscratch/drainford/ecDNA_data/*.bai /vscratch/drainford/ecDNA_data/*.parcel
