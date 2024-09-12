974e44b08cfd1d1a286f87aa4b4c5bd4847ca2d4
daverainford <rainford.d.k@gmail.com>
2024-09-12 12:33:17 -0700

    Added ega_download.sh to download ICGC through the EGA client.

---
+++ ega_download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=ega_client
+#SBATCH --mail-type=ALL     
+#SBATCH --mail-user=drainford@tgen.org  
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=40                                    
+#SBATCH --time=96:00:00               
+#SBATCH --output=/scratch/drainford/logs/ega_client.log
+
+module load singularity
+
+singularity exec pyega3_5.2.0--pyhdfd78af_0.sif pyega3 \
+    -c 40 \
+    -cf /scratch/drainford/ecDNA_pipeline/download_data/credentials.json \
+    fetch EGAD00001002157 \
+    --output-dir /vscratch/drainford/icgc_data/wgs_bams/ \
