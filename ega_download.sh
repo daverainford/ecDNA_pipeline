26488ebcb1e0a831540bd7035c4a724cf6b12542
daverainford <rainford.d.k@gmail.com>
2024-09-12 13:54:59 -0700

    Added --output-dir arg for ega_download.sh.

--- ega_download.sh
+++ ega_download.sh
@@ -3,14 +3,14 @@
 #SBATCH --mail-type=ALL     
 #SBATCH --mail-user=drainford@tgen.org  
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=40                                    
+#SBATCH --cpus-per-task=20                                    
 #SBATCH --time=96:00:00               
 #SBATCH --output=/scratch/drainford/logs/ega_client.log
 
 module load singularity
 
 singularity exec pyega3_5.2.0--pyhdfd78af_0.sif pyega3 \
-    -c 40 \
+    -c 20 \
     -cf /scratch/drainford/ecDNA_pipeline/download_data/credentials.json \
     fetch EGAD00001002157 \
-    --output-dir /vscratch/drainford/icgc_data/wgs_bams/ \
+    --output-dir /scratch/drainford/ecDNA_pipeline/
