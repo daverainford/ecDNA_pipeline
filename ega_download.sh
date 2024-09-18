21315914b6e398127d5282ea64d0291b9e7a0487
David Rainford <rainford.d.k@gmail.com>
2024-09-18 09:11:08 -0700

    Update ega_download.sh
    Changed from downloading the whole project to downloading specific files.

--- ega_download.sh
+++ ega_download.sh
@@ -3,14 +3,21 @@
 #SBATCH --mail-type=ALL     
 #SBATCH --mail-user=drainford@tgen.org  
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=20                                    
+#SBATCH --cpus-per-task=40                                    
 #SBATCH --time=96:00:00               
 #SBATCH --output=/scratch/drainford/logs/ega_client.log
 
 module load singularity
 
-singularity exec pyega3_5.2.0--pyhdfd78af_0.sif pyega3 \
-    -c 20 \
-    -cf /scratch/drainford/ecDNA_pipeline/download_data/credentials.json \
-    fetch EGAD00001002157 \
-    --output-dir /scratch/drainford/ecDNA_pipeline/
+# Store EGA file IDs in variable from batch file
+files=$(cat /scratch/drainford/ecDNA_pipeline/download_data/pcawg_batch1.txt)
+
+# Iterate over file ids to download BAMs
+for file in ${files}
+do
+    singularity exec /scratch/drainford/containers/pyega3_5.2.0--pyhdfd78af_0.sif pyega3 \
+        -c 40 \
+        -cf /scratch/drainford/ecDNA_pipeline/download_data/credentials.json \
+        fetch ${file:0:15} \
+        --output-dir /scratch/drainford/ecDNA_pipeline/mela_au_wgs_bams/ 
+done
